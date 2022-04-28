8bb9b0cc5760ebe4b6c526a14443ce72452f1b21
leobozzi <lbozzi@gmail.com>
2022-04-28 18:30:34 -0300

    Initial commit

--- backbone_default/__manifest__.py
+++ backbone_default/__manifest__.py
@@ -245,7 +245,7 @@
     ],
 
     'git-repos': [
-        'https://github.com/leobozzi/cl-optilogic.git',
+        'https://github.com/leobozzi/cl-backbone.git',
         'https://github.com/leobozzi/odoo-addons-utils bit-odoo-addons-utils',
         'https://github.com/leobozzi/odoo-custom bit-odoo-custom',
 
